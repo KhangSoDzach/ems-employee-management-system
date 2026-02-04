f4560e268eaab22d820b2d232c9e7bbd8becb530
baohgce191304 <baohg.ce191304@gmail.com>
2026-02-04 15:32:41 +0700

    add UI Dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LoginPage } from "@/features/auth/LoginPage";
+import Dashboard from "./features/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
