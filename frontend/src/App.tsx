f30ae52341e88b3dbfe7e40090990debd3623089
sigma-69 <baolgce191178@gmail.com>
2026-02-25 16:12:52 +0700

    feat: translate employee dashboard & dynamic app-sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { LoginPage } from "@/features/auth/LoginPage";
 import Dashboard from "./features/Dashboard";
 import AddingPage from "./features/admin/AddingPage";
-
+import { ForgotPasswordPage } from "./features/auth/ForgotPasswordPage";
+import EmployeeDashboard from "./features/employee/EmployeeDashboard";
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +11,8 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/adding" element={<AddingPage />} />
+        <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+        <Route path="/employee" element={<EmployeeDashboard />} />
       </Routes>
     </BrowserRouter>
   );
