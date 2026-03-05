56620114c919d885240fd34e50124cd7b71985b5
Hương Mai <hmai0172@gmail.com>
2026-03-05 22:47:28 +0700

    Update AssetStateException.java

--- backend/src/main/java/com/company/ems/backend/asset/exception/AssetStateException.java
+++ backend/src/main/java/com/company/ems/backend/asset/exception/AssetStateException.java
@@ -1,4 +1,11 @@
 package com.company.ems.backend.asset.exception;
 
-public class AssetStateException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class AssetStateException extends RuntimeException {
+
+    public AssetStateException(String message) {
+        super(message);
+    }
 }
